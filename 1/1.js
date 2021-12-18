2057199f6894beb3480a0e0a9685e294d81fdf7f
Zahra <abbaszadeh.zhr@gmail.com>
2021-12-18 19:59:08 +0330

    softEngineeringClass

---
+++ 1/1.js
@@ -0,0 +1,8 @@
+function softEngineeringClass(className, classUnit, classCapacity) {
+    this.className = className
+    this.classUnit = classUnit
+    this.classCapacity = classCapacity
+}
+let result1 = new softEngineeringClass('softEngineering', 3, 50)
+console.log(result1);
+

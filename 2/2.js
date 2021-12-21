07832b0a8c4d370944c2b385565e1bba52478cdd
Zahra <abbaszadeh.zhr@gmail.com>
2021-12-21 21:35:35 +0330

    Media & Book

--- 2/2.js
+++ 2/2.js
@@ -1,11 +1,28 @@
-class media {
-
+class Media {
+    constructor (artMedia) {
+        this.mediaType = artMedia
+    }
+    description() {
+        return 'one of the List of art media is => ' + this.mediaType;
+    }
 }
 
-class book {
-
+class Book extends Media {
+    constructor (artMedia, name) {
+        super(artMedia);
+        this.bookName = name
+    }
+    show() {
+        return this.description() + ' , the name of the book : ' + this.bookName;
+    }
 }
 
-class film {
+class Film extends Media {
     
 }
+
+
+
+
+let myBook = new Book("book", "A fraction of the whole");
+console.log(myBook.show()); // one of the List of art media is => book , the name of the book : A fraction of the whole

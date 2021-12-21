6190babfd850aa07bc589404d00a448475eba074
Zahra <abbaszadeh.zhr@gmail.com>
2021-12-21 21:44:50 +0330

    Film class

--- 2/2.js
+++ 2/2.js
@@ -3,7 +3,7 @@ class Media {
         this.mediaType = artMedia
     }
     description() {
-        return 'one of the List of art media is => ' + this.mediaType;
+        return 'one of art media is => ' + this.mediaType;
     }
 }
 
@@ -18,11 +18,22 @@ class Book extends Media {
 }
 
 class Film extends Media {
-    
+    constructor (artMedia, name) {
+        super(artMedia);
+        this.filmName = name
+    }
+    introduce() {
+        return this.description() + ' , the name of the film : ' + this.filmName;
+    }
 }
 
 
 
 
 let myBook = new Book("book", "A fraction of the whole");
-console.log(myBook.show()); // one of the List of art media is => book , the name of the book : A fraction of the whole
+console.log(myBook.show()); 
+// one of art media is => book , the name of the book : A fraction of the whole
+
+let myFavoriteMovie = new Film("Cinema", "Gone With The Wind");
+console.log(myFavoriteMovie.introduce()); 
+// one of art media is => Cinema , the name of the film : Gone With The Wind
